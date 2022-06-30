28f327133b6db983df27658b1868339311a39263
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-06-30 17:56:10 +0400

    First Commit

---
+++ P225Allup/P225Allup/wwwroot/manage/js/custom.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    $(document).on('click', '.deleteandrestorebtn', function (e) {
+        e.preventDefault();
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch($(this).attr('href'))
+                    .then(res => res.text())
+                    .then(data => {
+                        $('.datacontainer').html(data)
+                    })
+                
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+    })
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr["success"]("I do not think that means what you think it means.")
+    /*Command: toastr["success"]("I do not think that means what you think it means.")*/
+
+    
+})
